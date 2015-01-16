1fd32164a39aac6e0b475c7b2c5103bea8d82491
Mac <mac@tests-MacBook-Pro.local>
2015-01-15 23:31:08 -0800

    Tip calculator project

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate  {
 
+    @IBOutlet weak var billamountText: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text="$ 0.00"
+        totalLabel.text="$ 0.00"
+        billamountText.delegate = self;
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +31,36 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onEditChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = (tipPercentages[tipControl.selectedSegmentIndex] as NSNumber).floatValue
+        
+        var billamount = (billamountText.text as NSString).floatValue
+        var tip = billamount * tipPercentage
+        var total = billamount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+
+    @IBAction func onTap(sender: AnyObject) {view.endEditing(true) }
+        
+    func textFieldDidBeginEditing(textField: UITextField) {
+        animateKeyBoard(true, moveValue: 100)
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        animateKeyBoard(false, moveValue: 100)
+    }
+    
+    func animateKeyBoard (up:Bool, moveValue :CGFloat) {
+        var movementDuration:NSTimeInterval = 0.3
+        var movement:CGFloat = ( up ? -moveValue : moveValue)
+        UIView.beginAnimations( "animateView", context: nil)
+        UIView.setAnimationBeginsFromCurrentState(true)
+        UIView.setAnimationDuration(movementDuration )
+        self.view.frame = CGRectOffset(self.view.frame, 0,  movement)
+        UIView.commitAnimations()
+    }
 }
 
